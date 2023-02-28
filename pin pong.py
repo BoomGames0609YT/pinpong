c7de285f35f95d8168d81215601dae06ba3712bb
BoomGames0609YT <125994859+BoomGames0609YT@users.noreply.github.com>
2023-02-28 20:51:51 +0500

    PingPong v.0.3.
    добавлены движения
    добавлены отображения
    добавлен мяч
    добавлено управление на двоих человек

--- pin pong.py
+++ pin pong.py
@@ -6,11 +6,11 @@ FPS = 60
 win = display.set_mode((win_w, win_h))
 
 class GameSprite(sprite.Sprite):
-    def __init__(self, player_image, player_x, player_y, sizw_x, size_y, player_speed):
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
         sprite.Sprite.__init__(self)
         self.image = transform.scale(image.load(player_image),(size_x, size_y))
         self.speed = player_speed
-        self.rect - self.image.get_rect()
+        self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
 
@@ -21,9 +21,9 @@ class Player(GameSprite):
     
     def update_l(self):
         keys = key.get_pressed()
-        if keys[K_UP] and self.rect.y > 5:
+        if keys[K_w] and self.rect.y > 5:
                 self.rect.y -= self.speed
-        if keys[K_DOWN] and self.rect.y < win_width - 80:
+        if keys[K_s] and self.rect.y < win_h - 150:
             self.rect.y += self.speed
 
 
@@ -31,20 +31,31 @@ class Player(GameSprite):
         keys = key.get_pressed()
         if keys[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_DOWN] and self.rect.y < win_width - 80:
+        if keys[K_DOWN] and self.rect.y < win_h - 150:
             self.rect.y += self.speed
 
+racket1 = Player('racket.png', 0, 200, 50, 150, 4)
+racket2 = Player('racket.png', 650, 200, 50, 150, 4)
+ball = GameSprite('tenis_ball.png', 200, 200, 50, 50, 4)
 
-
-
+finish = False
 game = True
 while game:
     for e in event.get():
         if e.type == QUIT:
             game == False
+            
+    if finish == False:
+        racket1.update_l()
+        racket2.update_r()
+       
+        win.fill((100,100,100))
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
 
 
-    win.fill((100,100,100))
+    
     
     display.update()
     clock.tick(FPS)
